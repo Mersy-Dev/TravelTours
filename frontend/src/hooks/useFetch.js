b0e1248a3eb3faf73a6fa48685452e551596cfe1
Mersy-Dev <anuoluwafalaju@gmail.com>
2023-11-22 21:04:12 +0100

    A modified axios fetch

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';  
+import axios from 'axios';
 
-const useFetch = url => {
+const useFetch = (initialUrl) => {
     const [data, setData] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
@@ -10,34 +10,26 @@ const useFetch = url => {
         const fetchData = async () => {
             setLoading(true);
 
-            // const url = 'http://localhost:4000/api/v1/tours/search/getFeaturedTours';
             try {
-                const res = await fetch(url);
+                const res = await axios.get(initialUrl);
 
-                if (!res.ok) {
+                if (res.status !== 200) {
                     setError('Could not fetch the data for that resource');
-                    // alert('Could not fetch the data for that resource')
+                } else {
+                    setData(res.data.data);
                 }
-                const result = await res.json();
-                setData(result.data);
+
                 setLoading(false);
             } catch (error) {
-                setError(error);
+                setError('Error fetching data');
                 setLoading(false);
-
             }
         };
 
         fetchData();
+    }, [initialUrl]);
 
-    }, [url]);
-
-
-    return {
-        data,
-        error,
-        loading
-    };
+    return { data, error, loading };
 };
 
-export default useFetch
+export default useFetch;
